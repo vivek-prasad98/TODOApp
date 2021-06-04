333ab221c0caec71d8cc5505aa14f9130a1b41db
vivek prasad <ppubg7158@gmail.com>
2021-06-04 21:23:27 +0530

    After some updation

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -2,13 +2,22 @@ import './../Assets/CSS/App.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faCheck,faTrash } from '@fortawesome/free-solid-svg-icons'
 
+function todosLeft(){
+  let count = 0;
+  for(let ele of JSON.parse(localStorage.getItem('Active'))){
+    if(!ele.Comp)
+      count += 1;
+  }
+  return count > 0 ? count : 'No';
+}
+
 export default function Footer(props){
     return (
         <div className='panel-heading panel-footer'>
           <div className='left'>
             <FontAwesomeIcon icon={faTrash} style={{ margin: '0px 10px' }} onClick={props.handlers[0]} />|
             <FontAwesomeIcon icon={faCheck} style={{ margin: '0px 10px' }} onClick={props.handlers[1]} /> |{' '}
-            <span> {localStorage.length+1} TODO Left</span>
+            <span> {todosLeft()} Todo Left</span>
           </div>
           <div className='right'>
             <button className='customBtn' onClick={props.handlers[2]}>All</button>
